c46f376dbb6570c951cd50941a7b38aa100af8af
maria <dalfonso@cern.ch>
2018-04-05 15:06:10 +0200

    ALCARECOHcalCalMinBiasHI inherit from HcalCalMinBias

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBiasHI_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBiasHI_cff.py
@@ -5,6 +5,7 @@ import FWCore.ParameterSet.Config as cms
 #------------------------------------------------
 
 from Calibration.HcalAlCaRecoProducers.ALCARECOHcalCalMinBiasNoise_cff import *
+from Calibration.HcalAlCaRecoProducers.ALCARECOHcalCalMinBias_cff import *
 
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 hcalminbiasHLT =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
@@ -15,36 +16,10 @@ hcalminbiasHLT =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
 
 import RecoLocalCalo.HcalRecProducers.HBHEPhase1Reconstructor_cfi
 
-import RecoLocalCalo.HcalRecProducers.hfsimplereco_cfi
-hfrecoMBNZS = RecoLocalCalo.HcalRecProducers.hfsimplereco_cfi.hfsimplereco.clone()
-
-hfrecoMBNZS.firstSample = 2
-hfrecoMBNZS.samplesToAdd = 2
-hfrecoMBNZS.digiLabel = 'hcalDigiAlCaMB'
-hfrecoMBNZS.tsFromDB = cms.bool(False)
-hfrecoMBNZS.dropZSmarkedPassed = cms.bool(False)
 
 seqALCARECOHcalCalMinBiasDigi = cms.Sequence(hcalminbiasHLT*hcalDigiAlCaMB*gtDigisAlCaMB)
 seqALCARECOHcalCalMinBias = cms.Sequence(hbherecoNoise*hfrecoNoise*hfrecoMBNZS*horecoNoise)
 
-import RecoLocalCalo.HcalRecProducers.hfprereco_cfi
-hfprerecoMBNZS = RecoLocalCalo.HcalRecProducers.hfprereco_cfi.hfprereco.clone(
-    digiLabel = cms.InputTag("hcalDigiAlCaMB"),
-    dropZSmarkedPassed = cms.bool(False),
-    tsFromDB = cms.bool(False),
-    sumAllTimeSlices = cms.bool(True),
-    forceSOI = cms.int32(1)
-)
-
-import RecoLocalCalo.HcalRecProducers.HFPhase1Reconstructor_cfi
-_phase1_hfrecoMBNZS = RecoLocalCalo.HcalRecProducers.HFPhase1Reconstructor_cfi.hfreco.clone(
-    inputLabel = cms.InputTag("hfprerecoMBNZS"),
-    setNoiseFlags = cms.bool(False),
-    algorithm = dict(
-        Class = cms.string("HFSimpleTimeCheck"),
-        rejectAllFailures = cms.bool(False)
-    ),
-)
 
 _phase1_seqALCARECOHcalCalMinBias = seqALCARECOHcalCalMinBias.copy()
 _phase1_seqALCARECOHcalCalMinBias.insert(0,hfprerecoMBNZS)
@@ -52,17 +27,15 @@ _phase1_seqALCARECOHcalCalMinBias.insert(0,hfprerecoNoise)
 
 from Configuration.Eras.Modifier_run2_HF_2017_cff import run2_HF_2017
 run2_HF_2017.toReplaceWith( seqALCARECOHcalCalMinBias, _phase1_seqALCARECOHcalCalMinBias )
-run2_HF_2017.toReplaceWith( hfrecoNoise, _phase1_hfrecoNoise )
-run2_HF_2017.toReplaceWith( hfrecoMBNZS, _phase1_hfrecoMBNZS )
-
-import RecoLocalCalo.HcalRecProducers.hbheplan1_cfi
-hbheplan1Noise = RecoLocalCalo.HcalRecProducers.hbheplan1_cfi.hbheplan1.clone(
-    hbheInput = cms.InputTag("hbheprerecoNoise")
-)
+run2_HF_2017.toReplaceWith( hfrecoNoise, phase1_hfrecoNoise )
+run2_HF_2017.toReplaceWith( hfrecoMBNZS, phase1_hfrecoMBNZS )
 
 _plan1_seqALCARECOHcalCalMinBias = _phase1_seqALCARECOHcalCalMinBias.copy()
 hbheprerecoNoise = hbherecoNoise.clone()
+hbheprerecoMBNZS = hbherecoMBNZS.clone()
 _plan1_seqALCARECOHcalCalMinBias.insert(0,hbheprerecoNoise)
+_plan1_seqALCARECOHcalCalMinBias.insert(0,hbheprerecoMBNZS)
 from Configuration.Eras.Modifier_run2_HEPlan1_2017_cff import run2_HEPlan1_2017
 run2_HEPlan1_2017.toReplaceWith(hbherecoNoise, hbheplan1Noise)
+run2_HEPlan1_2017.toReplaceWith(hbherecoMBNZS, hbheplan1MBNZS)
 run2_HEPlan1_2017.toReplaceWith(seqALCARECOHcalCalMinBias, _plan1_seqALCARECOHcalCalMinBias)
