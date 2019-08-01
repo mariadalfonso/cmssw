f131e5fed5a7d4834dc3320bef95c138d5a2811b
maria <dalfonso@cern.ch>
2019-08-01 15:58:39 +0200

    fix for NOSE
    last changes for pre4
    
    fixes from Sunanda
    
    small notes
    
    resolveConflict

--- DataFormats/ForwardDetId/src/HFNoseTriggerDetId.cc
+++ DataFormats/ForwardDetId/src/HFNoseTriggerDetId.cc
@@ -1,94 +1,100 @@
 #include "DataFormats/ForwardDetId/interface/HFNoseTriggerDetId.h"
+#include "DataFormats/ForwardDetId/interface/HFNoseDetId.h"
 #include "FWCore/Utilities/interface/Exception.h"
 #include <ostream>
 #include <iostream>
 
-const HFNoseTriggerDetId HFNoseTriggerDetId::Undefined(HFNose,0,0,0,0,0,0,0);
+const HFNoseTriggerDetId HFNoseTriggerDetId::Undefined(HFNoseTrigger, 0, 0, 0, 0, 0, 0, 0);
 
-HFNoseTriggerDetId::HFNoseTriggerDetId() : DetId() {
-}
+HFNoseTriggerDetId::HFNoseTriggerDetId() : DetId() {}
 
-HFNoseTriggerDetId::HFNoseTriggerDetId(uint32_t rawid) : DetId(rawid) {
-}
+HFNoseTriggerDetId::HFNoseTriggerDetId(uint32_t rawid) : DetId(rawid) {}
 
-HFNoseTriggerDetId::HFNoseTriggerDetId(int subdet, int zp, int type, int layer,
-				     int waferU, int waferV, int cellU,
-				     int cellV) : DetId(HGCalTrigger,ForwardEmpty) {  
-
+HFNoseTriggerDetId::HFNoseTriggerDetId(
+    int subdet, int zp, int type, int layer, int waferU, int waferV, int cellU, int cellV)
+    : DetId(HGCalTrigger, ForwardEmpty) {
   int waferUabs(std::abs(waferU)), waferVabs(std::abs(waferV));
   int waferUsign = (waferU >= 0) ? 0 : 1;
   int waferVsign = (waferV >= 0) ? 0 : 1;
-  int zside      = (zp < 0) ? 1 : 0;
-  id_ |= (((cellU     & kHFNoseCellUMask) << kHFNoseCellUOffset) |
-	  ((cellV     & kHFNoseCellVMask) << kHFNoseCellVOffset) |
-	  ((waferUabs & kHFNoseWaferUMask) << kHFNoseWaferUOffset) |
-	  ((waferUsign& kHFNoseWaferUSignMask) << kHFNoseWaferUSignOffset) |
-	  ((waferVabs & kHFNoseWaferVMask) << kHFNoseWaferVOffset) |
-	  ((waferVsign& kHFNoseWaferVSignMask) << kHFNoseWaferVSignOffset) |
-	  ((layer     & kHFNoseLayerMask) << kHFNoseLayerOffset) |
-	  ((zside     & kHFNoseZsideMask) << kHFNoseZsideOffset) |
-	  ((type      & kHFNoseTypeMask)  << kHFNoseTypeOffset) |
-	  ((subdet    & kHFNoseSubdetMask)<< kHFNoseSubdetOffset));
+  int zside = (zp < 0) ? 1 : 0;
+  id_ |= (((cellU & kHFNoseCellUMask) << kHFNoseCellUOffset) | ((cellV & kHFNoseCellVMask) << kHFNoseCellVOffset) |
+          ((waferUabs & kHFNoseWaferUMask) << kHFNoseWaferUOffset) |
+          ((waferUsign & kHFNoseWaferUSignMask) << kHFNoseWaferUSignOffset) |
+          ((waferVabs & kHFNoseWaferVMask) << kHFNoseWaferVOffset) |
+          ((waferVsign & kHFNoseWaferVSignMask) << kHFNoseWaferVSignOffset) |
+          ((layer & kHFNoseLayerMask) << kHFNoseLayerOffset) | ((zside & kHFNoseZsideMask) << kHFNoseZsideOffset) |
+          ((type & kHFNoseTypeMask) << kHFNoseTypeOffset) | ((subdet & kHFNoseSubdetMask) << kHFNoseSubdetOffset));
 }
 
 HFNoseTriggerDetId::HFNoseTriggerDetId(const DetId& gen) {
   if (!gen.null()) {
-    if ((gen.det()!=DetId::HGCalTrigger) || gen.subdetId()!=HGCalTriggerSubdetector::HFNoseTrigger) {
-      throw cms::Exception("Invalid DetId") << "Cannot initialize HFNoseTriggerDetId from " << std::hex << gen.rawId() << std::dec; 
-    }  
+    if ((gen.det() != HGCalTrigger) || ((gen.subdetId() & kHFNoseSubdetMask) != HFNoseTrigger)) {
+      throw cms::Exception("Invalid DetId")
+          << "Cannot initialize HFNoseTriggerDetId from " << std::hex << gen.rawId() << std::dec;
+    }
   }
   id_ = gen.rawId();
 }
 
 HFNoseTriggerDetId& HFNoseTriggerDetId::operator=(const DetId& gen) {
   if (!gen.null()) {
-    if ((gen.det()!=DetId::HGCalTrigger) || gen.subdetId()!=HGCalTriggerSubdetector::HFNoseTrigger) {
-      throw cms::Exception("Invalid DetId") << "Cannot assign HFNoseTriggerDetId from " << std::hex << gen.rawId() << std::dec; 
-    }  
+    if ((gen.det() != HGCalTrigger) || ((gen.subdetId() & kHFNoseSubdetMask) != HFNoseTrigger)) {
+      throw cms::Exception("Invalid DetId")
+          << "Cannot assign HFNoseTriggerDetId from " << std::hex << gen.rawId() << std::dec;
+    }
   }
   id_ = gen.rawId();
   return (*this);
 }
 
 int HFNoseTriggerDetId::triggerCellX() const {
-  // HFNose those need to be redefined
-  int N = 12;
-  return (3 * (triggerCellV() - N) + 2);
-
+  int nT =
+      (type() == HFNoseDetId::HFNoseFine) ? HFNoseDetId::HFNoseFineTrigger : HFNoseDetId::HFNoseCoarseTrigger;
+  int N = nT * HFNoseTriggerCell;
+  std::vector<int> vc = cellV();
+  int x(0);
+  for (auto const& v : vc) {
+    x += (3 * (v - N) + 2);
+  }
+  return (x / vc.size());
 }
 
 int HFNoseTriggerDetId::triggerCellY() const {
-  // HFNose those need to be redefined
-  int N = 12;
-  return (2 * triggerCellU() - (N + triggerCellV()));
-
+  int nT =
+      (type() == HFNoseDetId::HFNoseFine) ? HFNoseDetId::HFNoseFineTrigger : HFNoseDetId::HFNoseCoarseTrigger;
+  int N = nT * HFNoseTriggerCell;
+  std::vector<int> uc = cellU();
+  std::vector<int> vc = cellV();
+  int y(0);
+  for (unsigned int k = 0; k < uc.size(); ++k) {
+    y += (2 * uc[k] - (N + vc[k]));
+  }
+  return (y / vc.size());
 }
 
 std::vector<int> HFNoseTriggerDetId::cellU() const {
-  // HFNose those need to be redefined
   std::vector<int> uc;
-  int nT = 1.;
-  if ((triggerCellU() >= HGCalTriggerCell) && 
-      (triggerCellV() >= HGCalTriggerCell)) {
-    int u0 = nT*triggerCellU(); 
-    for (int i=0; i<nT; ++i) {
-      for (int j=0; j<nT; ++j) {
-	uc.emplace_back(u0+i);
+  int nT =
+      (type() == HFNoseDetId::HFNoseFine) ? HFNoseDetId::HFNoseFineTrigger : HFNoseDetId::HFNoseCoarseTrigger;
+  if ((triggerCellU() >= HFNoseTriggerCell) && (triggerCellV() >= HFNoseTriggerCell)) {
+    int u0 = nT * triggerCellU();
+    for (int i = 0; i < nT; ++i) {
+      for (int j = 0; j < nT; ++j) {
+        uc.emplace_back(u0 + i);
       }
     }
-  } else if ((triggerCellU() < HGCalTriggerCell) && 
-	     (triggerCellU() <= triggerCellV())) {
-    int u0 = nT*triggerCellU(); 
-    for (int i=0; i<nT; ++i) {
-      for (int j=0; j<nT; ++j) {
-	uc.emplace_back(u0+i);
+  } else if ((triggerCellU() < HFNoseTriggerCell) && (triggerCellU() <= triggerCellV())) {
+    int u0 = nT * triggerCellU();
+    for (int i = 0; i < nT; ++i) {
+      for (int j = 0; j < nT; ++j) {
+        uc.emplace_back(u0 + i);
       }
     }
   } else {
-    int u0 = nT*(triggerCellU()-1)+1;
-    for (int i=0; i<nT; ++i) {
-      for (int j=0; j<nT; ++j) {
-	uc.emplace_back(u0+j);
+    int u0 = nT * (triggerCellU() - 1) + 1;
+    for (int i = 0; i < nT; ++i) {
+      for (int j = 0; j < nT; ++j) {
+        uc.emplace_back(u0 + j);
       }
       ++u0;
     }
@@ -97,58 +103,49 @@ std::vector<int> HFNoseTriggerDetId::cellU() const {
 }
 
 std::vector<int> HFNoseTriggerDetId::cellV() const {
-  // HFNose those need to be redefined
   std::vector<int> vc;
-  int nT = 2.;
-  if ((triggerCellU() >= HGCalTriggerCell) && 
-      (triggerCellV() >= HGCalTriggerCell)) {
-    int v0 = nT*triggerCellV(); 
-    for (int i=0; i<nT; ++i) {
-      for (int j=0; j<nT; ++j) {
-	vc.emplace_back(v0+j);
+  int nT =
+      (type() == HFNoseDetId::HFNoseFine) ? HFNoseDetId::HFNoseFineTrigger : HFNoseDetId::HFNoseCoarseTrigger;
+  if ((triggerCellU() >= HFNoseTriggerCell) && (triggerCellV() >= HFNoseTriggerCell)) {
+    int v0 = nT * triggerCellV();
+    for (int i = 0; i < nT; ++i) {
+      for (int j = 0; j < nT; ++j) {
+        vc.emplace_back(v0 + j);
       }
     }
-  } else if ((triggerCellU() < HGCalTriggerCell) && 
-	     (triggerCellU() <= triggerCellV())) {
-    int v0 = nT*triggerCellV(); 
-    for (int i=0; i<nT; ++i) {
-      for (int j=0; j<nT; ++j) {
-	vc.emplace_back(v0+j);
+  } else if ((triggerCellU() < HFNoseTriggerCell) && (triggerCellU() <= triggerCellV())) {
+    int v0 = nT * triggerCellV();
+    for (int i = 0; i < nT; ++i) {
+      for (int j = 0; j < nT; ++j) {
+        vc.emplace_back(v0 + j);
       }
       ++v0;
     }
   } else {
-    int v0 = nT*triggerCellV();
-    for (int i=0; i<nT; ++i) {
-      for (int j=0; j<nT; ++j) {
-	vc.emplace_back(v0+i);
+    int v0 = nT * triggerCellV();
+    for (int i = 0; i < nT; ++i) {
+      for (int j = 0; j < nT; ++j) {
+        vc.emplace_back(v0 + i);
       }
     }
   }
   return vc;
 }
 
-std::vector<std::pair<int,int> > HFNoseTriggerDetId::cellUV() const {
-  // HFNose those need to be redefined
+std::vector<std::pair<int, int> > HFNoseTriggerDetId::cellUV() const {
   std::vector<int> uc = cellU();
   std::vector<int> vc = cellV();
-  std::vector<std::pair<int,int> > uv;
-  for (unsigned int k=0; k<uc.size(); ++k) {
-    uv.emplace_back(std::pair<int,int>(uc[k],vc[k]));
+  std::vector<std::pair<int, int> > uv;
+  for (unsigned int k = 0; k < uc.size(); ++k) {
+    uv.emplace_back(std::pair<int, int>(uc[k], vc[k]));
   }
   return uv;
 }
 
-std::ostream& operator<<(std::ostream& s,const HFNoseTriggerDetId& id) {
-  // HFNose those need to be redefined
-  return s << " EE:HSil= " << id.isEE() << ":" << id.isHSilicon()
-	   << " type= " << id.type()  << " z= " << id.zside() 
-	   << " layer= " << id.layer() 
-	   << " wafer(u,v:x,y)= (" << id.waferU() << "," << id.waferV() << ":"
-	   << id.waferX() << "," << id.waferY() << ")"
-	   << " triggerCell(u,v:x,y)= (" << id.triggerCellU() << "," 
-	   << id.triggerCellV() << ":" << id.triggerCellX() << "," 
-	   << id.triggerCellY() << ")";
+std::ostream& operator<<(std::ostream& s, const HFNoseTriggerDetId& id) {
+  return s << " EE:HSil= " << id.isEE() << ":" << id.isHSilicon() << " type= " << id.type() << " z= " << id.zside()
+           << " layer= " << id.layer() << " wafer(u,v:x,y)= (" << id.waferU() << "," << id.waferV() << ":"
+           << id.waferX() << "," << id.waferY() << ")"
+           << " triggerCell(u,v:x,y)= (" << id.triggerCellU() << "," << id.triggerCellV() << ":" << id.triggerCellX()
+           << "," << id.triggerCellY() << ")";
 }
-
-
