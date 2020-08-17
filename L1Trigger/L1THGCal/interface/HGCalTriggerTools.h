b112d2722448998e4df520ce22a23ef94f877b8e
maria <dalfonso@cern.ch>
2020-08-17 10:17:28 +0200

    update lastLayerEE for HFnose

--- L1Trigger/L1THGCal/interface/HGCalTriggerTools.h
+++ L1Trigger/L1THGCal/interface/HGCalTriggerTools.h
@@ -51,7 +51,7 @@ public:
   // in the v8 geometry detid scheme
   int thicknessIndex(const DetId&, bool tc = false) const;
 
-  unsigned lastLayerEE() const { return eeLayers_; }
+  unsigned lastLayerEE(bool nose = false) const { return (nose ? HFNoseDetId::HFNoseLayerEEmax : eeLayers_); }
   unsigned lastLayerFH() const { return eeLayers_ + fhLayers_; }
   unsigned lastLayerBH() const { return totalLayers_; }
   unsigned lastLayerNose() const { return noseLayers_; }
